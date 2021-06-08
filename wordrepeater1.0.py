ac97c308389b4ef0d78d69cfd77748bf96e348be
echung-79 <83886240+echung-79@users.noreply.github.com>
2021-06-08 14:19:15 -0400

    Add files via upload

---
+++ wordrepeater1.0.py
@@ -0,0 +1,23 @@
+def word_repeater():
+    '''Function to do Word Repeater'''
+    word = input("Enter Which Word You'd Like to Repeat\n")
+    times = input(f"Enter How Many Times You'd Like to Repeat {word} as an integer.\n")
+    i_times = int(times)
+    ticker = 0
+    while ticker < i_times:
+        count = ticker + 1
+        print(f"{count}." + f" {word}")
+        ticker = ticker + 1
+    print("All Done!")
+    def do_again():
+        """Function to repeat or exit program"""
+        again = input("Press 1 to do again, Press 2 to Exit.\n")
+        if again == "1":
+            word_repeater()
+        elif again == "2":
+            exit()
+        else:
+            input("Invalid response, press enter to try again.\n")
+            do_again()
+    do_again()
+word_repeater()
